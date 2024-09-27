4aa311d98ad3d218eda27eaf21af9354512f0ff2
veretennikovalexey <raidex@yandex.ru>
2024-09-27 10:41:15 +0300

    alex

---
+++ pp.cmd
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x 
+
+git pull
+
+git config --global user.name veretennikovalexey
+
+git config --global user.email raidex@yandex.ru
+
+git add .
+git commit -m alex
+git push origin main
+
+pause
